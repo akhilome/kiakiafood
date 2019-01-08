8ad0cf9fea1b8ca904e86ce070aa8452e6d16d34
Kizito Akhilome <hovkard@gmail.com>
2019-01-08 20:13:55 +0100

    chore(setup-redux): setup redux for project
    - install redux, react-redux, redux-thunk
    - setup redux folder structure
    
    [Finishes #162953523]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+import App from './components/App';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+const jsx = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.querySelector('#app'));
