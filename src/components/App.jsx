14ec8901d015a1dc609ec754aae54f686ae11f10
Kizito Akhilome <kizito@akhilo.me>
2019-01-08 19:57:47 +0100

    chore(setup-react): set up empty react project (#1)
    - install react and react-dom
    - install and configure webpack
    - install and configure babel with webpack
    
    [Finishes #162953485]

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import './../index.css';
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const App = () => {
+  return <div>Kiakia Foodie 🍹🥘</div>;
+};
+
+export default hot(module)(App);
---
+++ webpack.config.dev.js
@@ -0,0 +1,10 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+
+module.exports = merge(baseConfig, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    port: 5000
+  }
+});
