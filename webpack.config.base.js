b7db6a912ae4a9d55a0c90079f1fa688d63e14c3
Kizito Akhilome <hovkard@gmail.com>
2019-01-08 19:34:16 +0100

    chore(setup-react): set up empty react project
    - install react and react-dom
    - install and configure webpack
    - install and configure babel with webpack
    
    [Finishes #162953485]

---
+++ webpack.config.base.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
