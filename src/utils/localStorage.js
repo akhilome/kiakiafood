403a6b4e5247991df3bc7c89e707f2d47ffb4c47
Kizito Akhilome <hovkard@gmail.com>
2019-02-06 05:01:06 +0100

    ch(feedback): abstract localstorage calls to utils

--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -4,3 +4,6 @@ export const saveToken = (token) => {
 
 export const getToken = () => window.localStorage.getItem('kiakiafoodToken') || '';
 export const removeToken = () => window.localStorage.removeItem('kiakiafoodToken');
+export const getCartItems = () => JSON.parse(window.localStorage.getItem('cart')) || {};
+export const updateCartItems = updatedCart => window.localStorage.setItem('cart', JSON.stringify(updatedCart));
+export const clearCartItems = () => window.localStorage.removeItem('cart');
